cba0121b05c930eb6dfe04ded8d866a74bb3e921
Duke09 <debopriyo09@outlook.com>
2021-01-18 07:51:21 +0530

    plus btn update

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -31,14 +31,12 @@ function addElement() {
 }
 
 function display() {
-    document.querySelector(".links-container").innerHTML = '<div class="col-2 order-2 my-2 addBtn"><a href="#Add_Doctors" data-toggle="modal" class="btn"><img src="icons/plus.svg" alt="" width="30"></a><!-- <input type="button" class="searchButton btn btn-primary float-right" onclick="addElement()" value="Add"> --></div>';
+    document.querySelector(".links-container").innerHTML = '<div class="col-2 my-2 order-2 addBtn"><a href="#Add_Doctors" data-toggle="modal" class="btn"><img src="icons/plus.svg" alt="" width="20"></a><!-- <input type="button" class="searchButton btn btn-primary float-right" onclick="addElement()" value="Add"> --></div>';
     for (var i = 0; i < elements.length; i++)
         document.querySelector(".links-container").innerHTML +=
-        "<div class='col-2 my-2 stored-links'>" +
+        "<div class='col-2 my-2 mr-3 stored-links'>" +
             "<a class='tile' href='https://" + elements[i][1] + "'>" +
-                "<div class='tile-icon'>" + 
-                    "<img width='25' height='25' src='" + elements[i][2] + "'>" + 
-                "</div>" + 
+                "<img width='20' height='20' src='" + elements[i][2] + "'>" + 
             "</a>" +
             "<img hidden width='25' height='25' id='dustbin' class='dustbin' " +
             " src = 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/user-trash-full-icon.png'" +
