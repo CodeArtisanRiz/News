8ae2bb530169a2dc556c867fb0734b0dc8943290
Duke09 <debopriyo09@outlook.com>
2021-01-18 07:17:36 +0530

    plus btn update

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -31,16 +31,19 @@ function addElement() {
 }
 
 function display() {
-    document.querySelector(".links-container").innerHTML = "";
+    document.querySelector(".links-container").innerHTML = '<div class="col-2 order-2 my-2 addBtn"><a href="#Add_Doctors" data-toggle="modal" class="btn"><img src="icons/plus.svg" alt="" width="30"></a><!-- <input type="button" class="searchButton btn btn-primary float-right" onclick="addElement()" value="Add"> --></div>';
     for (var i = 0; i < elements.length; i++)
         document.querySelector(".links-container").innerHTML +=
-        "<div class='col-3 my-2 stored-links'>" +
-        "<a class='tile' href='https://" + elements[i][1] + "'><div class='tile-icon'><img width='30' height='30' src='" +
-        elements[i][2] + "'></div></a>" +
-        "<img hidden width='25' height='25' id='dustbin' class='dustbin' " +
-        " src = 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/user-trash-full-icon.png'" +
-        " onclick='del(" + i + ")'>" +
-        "</div> </div>"
+        "<div class='col-2 my-2 stored-links'>" +
+            "<a class='tile' href='https://" + elements[i][1] + "'>" +
+                "<div class='tile-icon'>" + 
+                    "<img width='25' height='25' src='" + elements[i][2] + "'>" + 
+                "</div>" + 
+            "</a>" +
+            "<img hidden width='25' height='25' id='dustbin' class='dustbin' " +
+            " src = 'https://icons.iconarchive.com/icons/papirus-team/papirus-status/512/user-trash-full-icon.png'" +
+            " onclick='del(" + i + ")'>" +
+        "</div>"
 
 }
 
